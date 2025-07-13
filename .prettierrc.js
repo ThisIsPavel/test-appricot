252987cc1565e43ac9f44054c2408ef3f4f3c7bd
Pavel <Pav_kaf@mail.ru>
2025-07-14 01:55:08 +0300

    config: add prettierc & eslint rules

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  plugins: ['prettier-plugin-tailwindcss'],
+  tailwindConfig: './tailwind.config.js',
+};
