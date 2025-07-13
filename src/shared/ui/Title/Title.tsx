329ea606f253ea1ea3303263983c524ad5c4a508
Pavel <Pav_kaf@mail.ru>
2025-07-14 01:58:27 +0300

    feat: add title component

---
+++ src/shared/ui/Title/Title.tsx
@@ -0,0 +1,20 @@
+import { HTMLAttributes, ReactNode } from 'react';
+
+interface Props extends HTMLAttributes<HTMLElement> {
+  variant: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+  children: ReactNode;
+  className?: string;
+  isError?: boolean;
+}
+
+export default function Title({ variant, children, className, isError, ...props }: Props) {
+  const Tag = variant;
+  return (
+    <Tag
+      className={`${className} mb-8 text-6xl font-bold ${isError ? 'text-red-500 dark:text-red-500' : 'text-black dark:text-white'}`}
+      {...props}
+    >
+      {children}
+    </Tag>
+  );
+}
